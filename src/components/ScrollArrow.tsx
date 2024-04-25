6e077fe97ce3cd6c16c0eeccf32f481a246a8121
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-04-25 16:54:40 -0300

    feat: created component disappear on scroll function

--- src/components/ScrollArrow.tsx
+++ src/components/ScrollArrow.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function ScrollArrow() {
+  const [isVisible, setIsVisible] = useState(true);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const element = ref.current;
+      if (element) {
+        const { top } = element.getBoundingClientRect();
+        setIsVisible(top > window.innerHeight)
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
   return (
-    <div className="scrollWrapper">
+    <div ref={ref} className={`scrollWrapper ${isVisible ? "opacity-100" : "opacity-0"}`} >
       <span className="arrowSpan"></span>
       <span className="arrowSpan"></span>
       <span className="arrowSpan"></span>
