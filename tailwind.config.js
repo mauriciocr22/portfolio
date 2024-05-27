484fd597a8ea0581924a956d1eba015697e4bf86
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-05-27 17:01:12 -0300

    feat: importing fonts and creating animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        "spartan": ["League Spartan", "sans-serif"]
+        spartan: ["League Spartan", "sans-serif"],
+        canada: ["Radio Canada Big", "sans-serif"],
       },
       animation: {
-        "scroll": "animate 2s infinite"
+        scroll: "animate 2s infinite",
       },
       keyframes: {
         animate: {
-          "0%": { "opacity": 0, transform: "rotate(45deg) translate(-20px,-20px)" },
-          "50%": { "opacity": 1 },
-          "100%": { "opacity": 0, transform: "rotate(45deg) translate(20px,20px)" }
-        }
-      }
+          "0%": {
+            opacity: 0,
+            transform: "rotate(45deg) translate(-20px,-20px)",
+          },
+          "50%": { opacity: 1 },
+          "100%": {
+            opacity: 0,
+            transform: "rotate(45deg) translate(20px,20px)",
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
