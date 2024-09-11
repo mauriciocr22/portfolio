ee5f2dbef1b0564d476ea5134ce6d686d8177c69
Maurício de Carvalho <mauriciocr223@gmail.com>
2024-09-11 06:45:45 -0300

    style: fixing contact text

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
       id="contact"
       className="w-full flex flex-col items-center px-7 dark:bg-[#222222] pt-12"
     >
-      <h2 className="text-2xl text-slate-600 font-semibold mb-6 font-canada dark:text-slate-200">
+      <h2 className="text-2xl text-slate-600 font-semibold mb-6 font-canada dark:text-slate-200 text-center">
         {t("contactText")}
       </h2>
       <div className="flex gap-2 mb-10 contact">
